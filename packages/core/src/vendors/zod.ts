acbe78edcda8c399e9616dde974a8e8bcfa73e61
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-06-01 13:31:27 -0400

    Package: Zod 4 support
    Add ability to transform zod 4 schemas to JSON schemas using the
    native function.
    
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- packages/core/src/vendors/zod.ts
+++ packages/core/src/vendors/zod.ts
@@ -1,15 +1,41 @@
 import type { JSONSchema7 } from "json-schema";
-import type { ZodSchema } from "zod";
+import type { ZodTypeAny } from "zod";
+import * as z4 from "zod/v4/core";
 import { tryImport, type ToJsonSchemaFn } from "./index.js";
 
+type Schema = ZodTypeAny | z4.$ZodType
+
 export const getToJsonSchemaFn = async (): Promise<ToJsonSchemaFn> => {
-  const { zodToJsonSchema } = await tryImport(
-    import("zod-to-json-schema"),
-    "zod-to-json-schema",
-  );
-  return (schema, options) =>
-    zodToJsonSchema(
-      schema as ZodSchema<unknown>,
-      options as Record<string, unknown>,
-    ) as JSONSchema7;
+  return async (schema, options) => {
+    const zodSchema = schema as Schema;
+
+    // Zod 4 or 3
+    if ("_zod" in zodSchema) {
+      return z4.toJSONSchema(
+        zodSchema as z4.$ZodType,
+        {
+          target: "draft-7",
+          io: "input",
+          override: (ctx) => {
+            // Flattens nested allOfs with intersections
+            if (ctx.jsonSchema.allOf) {
+              ctx.jsonSchema.allOf = ctx.jsonSchema.allOf.flatMap(schema => 
+                schema.allOf ? schema.allOf : [schema]
+              );
+            }
+          }
+        }
+      ) as JSONSchema7;
+    } else {
+      const { zodToJsonSchema } = await tryImport(
+        import("zod-to-json-schema"),
+        "zod-to-json-schema",
+      );
+
+      return zodToJsonSchema(
+        zodSchema as ZodTypeAny,
+        options as Record<string, unknown>,
+      ) as JSONSchema7;
+    }
+  }
 };
